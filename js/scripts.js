ff4ef66d8b5836862ee1818e1be51a6b76fe2e57
vladliena <sotnikovavladlena@gmail.com>
2023-02-02 22:29:45 +0200

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-function addition (){
-    let sum = 0;
-    return function counter (add){
-        sum += add
-        console.log(sum)
+function addition (n){
+    let sum = n;
+    return function (add){
+        sum = sum + add
         return sum
     };
 }
 
-const add = addition();
-add(3)
-add(5)
-add(20)
+const add = addition(3);
+add(5);
+add(20);
+
+// function addition (){
+//     let sum = 0;
+//     return function counter (add){
+//         sum += add
+//         return sum
+//     };
+// }
+//
+// const add = addition();
+// add(3)
+// add(5)
+// add(20)
